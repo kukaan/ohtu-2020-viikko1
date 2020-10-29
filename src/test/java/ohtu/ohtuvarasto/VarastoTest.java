a7c0274e0b6efef08115c7968a5e0a2327f9a7e4
kukaan <58886775+kukaan@users.noreply.github.com>
2020-10-29 14:09:49 +0200

    Varasto tests line coverage 100%

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -13,11 +13,13 @@ import static org.junit.Assert.*;
 public class VarastoTest {
 
     Varasto varasto;
+    Varasto epatyhjaVarasto;
     double vertailuTarkkuus = 0.0001;
 
     @Before
     public void setUp() {
         varasto = new Varasto(10);
+        epatyhjaVarasto = new Varasto(10, 2);
     }
 
     @Test
@@ -36,6 +38,23 @@ public class VarastoTest {
 
         // saldon pitäisi olla sama kun lisätty määrä
         assertEquals(8, varasto.getSaldo(), vertailuTarkkuus);
+        
+        epatyhjaVarasto.lisaaVarastoon(2.2);
+        assertEquals(4.2, epatyhjaVarasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void NegatiivinenLisaysEiMuutaSaldoa() {
+        varasto.lisaaVarastoon(-1);
+
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void ylisuuriLisaysAsettaaSaldonMaksimiin() {
+        varasto.lisaaVarastoon(12);
+
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
     }
 
     @Test
@@ -53,6 +72,28 @@ public class VarastoTest {
         double saatuMaara = varasto.otaVarastosta(2);
 
         assertEquals(2, saatuMaara, vertailuTarkkuus);
+
+        double saatuMaara2 = epatyhjaVarasto.otaVarastosta(2);
+
+        assertEquals(2, saatuMaara, vertailuTarkkuus);
+    }
+
+    @Test
+    public void negatiivinenOttaminenPalauttaaOikeanMaaran() {
+        varasto.lisaaVarastoon(2);
+
+        double saatuMaara = varasto.otaVarastosta(-2);
+
+        assertEquals(0, saatuMaara, vertailuTarkkuus);
+    }
+
+    @Test
+    public void ylisuuriOttaminenPalauttaaOikeanMaaran() {
+        varasto.lisaaVarastoon(2);
+
+        double saatuMaara = varasto.otaVarastosta(4);
+
+        assertEquals(2, saatuMaara, vertailuTarkkuus);
     }
 
     @Test
@@ -63,6 +104,43 @@ public class VarastoTest {
 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+
+        epatyhjaVarasto.otaVarastosta(2);
+
+        assertEquals(10, epatyhjaVarasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
+    @Test
+    public void konstruktoriLuoEpatyhjanVaraston() {
+        assertEquals(2, epatyhjaVarasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void uudellaEpatyhjallaVarastollaOikeaTilavuus() {
+        assertEquals(10, epatyhjaVarasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void konstruktoriLuoNegatiivisenTilanTyhjaksiVarastoksi() {
+        Varasto negatiivinenTila1 = new Varasto(-1);
+        Varasto negatiivinenTila2 = new Varasto(-1, -1);
+        assertEquals(0, negatiivinenTila1.getSaldo(), vertailuTarkkuus);
+        assertEquals(0, negatiivinenTila1.getSaldo(), vertailuTarkkuus);
+        assertEquals(0, negatiivinenTila2.paljonkoMahtuu(), vertailuTarkkuus);
+        assertEquals(0, negatiivinenTila2.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void uudellaYlitaydellaVarastollaOikeaSaldo() {
+        Varasto ylitaysi = new Varasto(10,12);
+        
+        assertEquals(10, ylitaysi.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void toStringPalauttaaOikeanMuodon() {
+        String s = epatyhjaVarasto.toString();
+        
+        assertEquals("saldo = 2.0, vielä tilaa 8.0", epatyhjaVarasto.toString());
+    }
 }
